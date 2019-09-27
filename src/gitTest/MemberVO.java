27235a4bd175a9cbaeaa4dca5936b0fd84b17b10
choiwj92 <chjcwj8892@naver.com>
2019-09-27 17:21:00 +0900

    hotfix membervo 수정

--- src/gitTest/MemberVO.java
+++ src/gitTest/MemberVO.java
@@ -4,25 +4,36 @@ public class MemberVO {
 	private String bno;
 	private String name;
 	private String age;
-	
+
 	public String getBno() {
 		return bno;
 	}
+
 	public void setBno(String bno) {
 		this.bno = bno;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getAge() {
 		return age;
 	}
+
 	public void setAge(String age) {
 		this.age = age;
 	}
-	
-	
+
+	@Override
+	public String toString() {
+		return "MemberVO [bno=" + bno + ", name=" + name + ", age=" + age + ", getBno()=" + getBno() + ", getName()="
+				+ getName() + ", getAge()=" + getAge() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode()
+				+ ", toString()=" + super.toString() + "]";
+	}
+
 }
